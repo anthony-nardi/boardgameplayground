1baf6fa33395137bcf4fa240b0eacc136f7a9658
Anthony Nardi <anthonygt@gmail.com>
2023-03-17 21:45:29 -0400

    import component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { FeaturesCards } from "../components/Main";
+import { Analytics } from '@vercel/analytics/react';
+
 export default function Home() {
   return (
     <>
       <FeaturesCards />
+      <Analytics />
     </>
   );
 }
